b6c5e5394c4d57061f8570745a0de14e1ef598b7
Suraj Nair <surajnair9698@gmail.com>
2026-03-31 19:47:45 +0530

    feat: add social media links to footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,24 @@
+import Image from "next/image";
+import Link from "next/link";
+
 function Footer() {
   return (
-    <div>
-      <div>@2026 Rinzler. All Rights Reserved</div>
-      <div></div>
+    <div className="flex justify-between p-2">
+      <div className="text-xs">@2026 Rinzler. All Rights Reserved</div>
+      <div className=" flex gap-2 opacity-40 cursor-pointer">
+        <Link href="https://www.facebook.com/">
+          <Image src="/1.png" width={15} height={100} alt="site logo" />
+        </Link>
+        <Link href="https://www.instagram.com">
+          <Image src="/2.png" width={15} height={100} alt="site logo" />
+        </Link>
+        <Link href="https://www.x.com">
+          <Image src="/3.png" width={15} height={100} alt="site logo" />
+        </Link>
+        <Link href="https://www.youtube.com">
+          <Image src="/4.png" width={15} height={100} alt="site logo" />
+        </Link>
+      </div>
     </div>
   );
 }
